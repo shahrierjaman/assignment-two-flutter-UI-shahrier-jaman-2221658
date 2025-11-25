21f6ecd8628d729d979295db3e4b473b3f8c5556
Shahrier Jaman <157677455+shahrierjaman@users.noreply.github.com>
2025-11-25 08:45:04 +0600

    home page 2nd part update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -121,28 +121,138 @@ class Home extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                // 1st Card
                 Expanded(
                   child: Container(
-                    height: 100,
-                    color: Colors.grey.shade200, // temporary background
+                    padding: EdgeInsets.all(12),
+                    height: 110,
                     margin: EdgeInsets.only(right: 8),
-                    // First card content will be added later
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(239, 243, 229, 245),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.send_outlined,
+                            size: 28,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "Transfer",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+
+                // 2nd Card
                 Expanded(
                   child: Container(
-                    height: 100,
-                    color: Colors.grey.shade200,
+                    padding: EdgeInsets.all(12),
+                    height: 110,
                     margin: EdgeInsets.symmetric(horizontal: 4),
-                    // Second card content will be added later
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(239, 243, 229, 245),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.receipt_rounded,
+                            size: 28,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "Pay Bills",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+
+                // 3rd Card
                 Expanded(
                   child: Container(
-                    height: 100,
-                    color: Colors.grey.shade200,
+                    padding: EdgeInsets.all(12),
+                    height: 110,
                     margin: EdgeInsets.only(left: 8),
-                    // Third card content will be added later
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(239, 243, 229, 245),
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.attach_money_outlined,
+                            size: 28,
+                            color: Colors.purple,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          "Invest",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
